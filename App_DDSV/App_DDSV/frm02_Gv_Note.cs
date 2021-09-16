778541c7e0b18fa30cc76c7689ca9b0082121990
MinhKhanh69 <phamkhanh692000@gmail.com>
2021-09-16 20:11:37 +0700

    update App_DDSV

---
+++ App_DDSV/App_DDSV/frm02_Gv_Note.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App_DDSV
+{
+    public partial class frm02_Gv_Note : Form
+    {
+        public frm02_Gv_Note()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_AddStudent_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ App_DDSV/App_DDSV/user2_Gv_Summary.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App_DDSV
+{
+    public partial class user2_Gv_Summary : UserControl
+    {
+        public user2_Gv_Summary()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Refresh_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_AddStudent_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_TenHP_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_MaHP_Search_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgv_Summary_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+    }
+}
